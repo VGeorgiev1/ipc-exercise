9ed37094a23e895fd51f8709b7056bad650d77ab
Vladislav <vladdislav.georgiev@gmail.com>
2017-12-11 12:40:13 -0800

    checking for the seed

--- cyclicRead.c
+++ cyclicRead.c
@@ -9,12 +9,12 @@
 int main()
 {
 	int shmFd = shm_open( "shared_buffer", O_RDONLY, 0 );
+	
 	if( shmFd == -1 )
 	{
 		perror("Can't open file");
 		return 1;
 	}
-
 	struct cyclic_buf* mem = mmap( NULL, sizeof(struct cyclic_buf), PROT_READ, MAP_SHARED, shmFd, 0 );
 	if( mem == NULL )
 	{
@@ -22,20 +22,25 @@ int main()
 		return -1;
 	}	
 
-	uint64_t cur_pos = 0;
+	uint64_t cur_pos = mem -> pos;
+	uint64_t prev_seed = verify((void *)mem->BUFFER[cur_pos - 1].arr); //get the seed of the previous element before we start reading 
 	printf("starting at %ld\n", cur_pos);
 	while( true )
 	{
-		while(mem -> pos ==cur_pos){};
+		while(mem -> pos == cur_pos){};
 		int64_t cur_seed = verify((void *)mem->BUFFER[cur_pos].arr);
 		if(cur_seed == -1){
 			printf("Error!");
 			break;
 		}
-		printf("Verified at %d with seed %d\n", cur_pos, cur_seed);
+		if(cur_seed != prev_seed +1){ // checking if the seed of the current element is 1 less than previous one 
+			printf("Wrong seed!");
+			break;
+		}
+		prev_seed +=1;
+		printf("Verified at %ld with seed %ld\n", cur_pos, cur_seed);
 		cur_pos++;
 		cur_pos %= 1000;
 	}
-
 	return 0;
 }
