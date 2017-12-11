1c1905118eb99f4133cf3001cc4c739d96f0e6bb
Vladislav <vladdislav.georgiev@gmail.com>
2017-12-11 12:58:06 -0800

    i think i fixed it xd

--- cyclicWrite.c
+++ cyclicWrite.c
@@ -2,45 +2,47 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdbool.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdbool.h>
 #include "cyclicBuf.h"
 #include "gen.h"
+
 int main()
 {
-	int shmFd = shm_open( "shared_buffer", O_RDONLY, 0 );
 	
+	int shmFd = shm_open( "shared_buffer", O_CREAT | O_RDWR, S_IRWXU );
 	if( shmFd == -1 )
 	{
 		perror("Can't open file");
 		return 1;
 	}
-	struct cyclic_buf* mem = mmap( NULL, sizeof(struct cyclic_buf), PROT_READ, MAP_SHARED, shmFd, 0 );
+
+	int res;
+	res = ftruncate(shmFd, sizeof(struct cyclic_buf));
+	if (res == -1)
+	{
+		perror("Can't truncate file");
+		return res;
+	}
+	
+	struct cyclic_buf* mem = mmap( NULL, sizeof(struct cyclic_buf), PROT_READ | PROT_WRITE, MAP_SHARED, shmFd, 0 );
 	if( mem == NULL )
 	{
 		perror("Can't mmap");
 		return -1;
+	}
+	
+	mem -> pos = 0;
+	uint32_t seed = 0;
+	while(true)
+	{
+		generate((void *)mem -> BUFFER[ mem -> pos].arr, seed);
+		seed++;
+		mem->pos++;
+		mem->pos %= 1000;
 	}	
 
-	uint64_t cur_pos = mem -> pos;
-	uint64_t prev_seed = verify((void *)mem->BUFFER[cur_pos - 1].arr); //get the seed of the previous element before we start reading 
-	printf("starting at %ld\n", cur_pos);
-	while( true )
-	{
-		while(mem -> pos == cur_pos){};
-		int64_t cur_seed = verify((void *)mem->BUFFER[cur_pos].arr);
-		if(cur_seed == -1){
-			printf("Error!");
-			break;
-		}
-		if(cur_seed != prev_seed +1){ // checking if the seed of the current element is 1 less than previous one 
-			printf("Wrong seed!");
-			break;
-		}
-		prev_seed +=1;
-		printf("Verified at %ld with seed %ld\n", cur_pos, cur_seed);
-		cur_pos++;
-		cur_pos %= 1000;
-	}
 	return 0;
 }
